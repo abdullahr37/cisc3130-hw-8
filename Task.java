905abf3b1143723dba2078d2e022f348cc64cb08
abdullahr37 <all.use.1908@gmail.com>
2026-05-13 14:13:52 -0400

    Create Task.java

---
+++ Task.java
@@ -0,0 +1,15 @@
+public class Task {
+
+    String name;
+    int priority;
+
+    public Task(String name, int priority) {
+        this.name = name;
+        this.priority = priority;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (Priority " + priority + ")";
+    }
+}
